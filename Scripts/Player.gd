c68595ea3ed7adcd5079509c17b341a030ec5d5c
vsmx427 <vsmx427@gmail.com>
2024-05-24 21:48:43 -0300

    Player Run

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,16 @@
-extends Node2D
 extends Area2D
 
+export var speed = 500
 onready var _animated_sprite = $AnimatedSprite
 
-func _process(_delta):
+var screen_size
+
+
+func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		_animated_sprite.play("run")
-	else:
-		_animated_sprite.stop()
+	else: _animated_sprite.stop()
+	
+func _ready():
+	screen_size = get_viewport_rect().size
+
