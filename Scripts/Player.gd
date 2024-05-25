5ed2ab85c8ee20ba95c6757035a9c6ffda8fab8b
vsmx427 <vsmx427@gmail.com>
2024-05-24 21:24:08 -0300

    GAME TOTM

---
+++ Scripts/Player.gd
@@ -0,0 +1,10 @@
+extends Node2D
+extends Area2D
+
+onready var _animated_sprite = $AnimatedSprite
+
+func _process(_delta):
+	if Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("run")
+	else:
+		_animated_sprite.stop()
