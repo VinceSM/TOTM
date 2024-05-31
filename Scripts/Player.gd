d710c0c9b49d9c23c7bd904e818c02994f988695
Vicente SM <vsmx427@gmail.com>
2024-05-31 00:04:25 -0300

    En Level1 mecanicas terminadas

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,8 +1,6 @@
 extends KinematicBody2D
 
 export var speed = 500
-#export var jump_force = -500  # Fuerza de salto para un pequeño salto
-#export var gravity = 500
 onready var _animated_sprite = $AnimatedSprite
 
 var velocity = Vector2()
@@ -14,11 +12,10 @@ func _ready():
 
 func _process(delta):
 	handle_input()
-	#apply_gravity(delta)
 	move_player(delta)
 
 func handle_input():
-	velocity.x = 0  # Resetear la velocidad horizontal cada frame
+	velocity = Vector2()  # Resetear la velocidad cada frame
 	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
@@ -31,21 +28,14 @@ func handle_input():
 	else:
 		_animated_sprite.stop()
 
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and not is_on_ceiling:
+		position.y -= 500  # Mover hacia arriba
 		_animated_sprite.flip_v = true  # Voltear sprite verticalmente (mirar hacia arriba)
 		is_on_ceiling = true
 	elif Input.is_action_just_pressed("ui_down") and is_on_ceiling:
+		position.y += 500  # Mover hacia abajo
 		_animated_sprite.flip_v = false  # No voltear sprite verticalmente (mirar hacia abajo)
 		is_on_ceiling = false
-
-	#if Input.is_action_just_pressed("ui_select"):
-	#	velocity.y = jump_force 
-	#elif is_on_ceiling:
-	#	velocity.y = -jump_force  # Salto hacia abajo desde el techo
-
-#func apply_gravity(delta):
-	#if not is_on_floor() and not is_on_ceiling:
-		#velocity.y += gravity * delta
 
 func move_player(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -57,13 +47,13 @@ func move_player(delta):
 		position.x = screen_size.x - get_animated_sprite_width()
 
 	# Restringir la posición vertical del jugador cuando esté en el suelo o en el techo
-	if is_on_floor() and position.y > screen_size.y - get_animated_sprite_height():
-		position.y = screen_size.y - get_animated_sprite_height()
-	elif is_on_ceiling() and position.y < 0:
-		position.y = 0
+	if is_on_ceiling:
+		position.y = max(0, position.y)
+	else:
+		position.y = min(screen_size.y - get_animated_sprite_height(), position.y)
 
 func is_on_floor():
-	return is_on_wall()  # Verificar si el jugador está en contacto con el suelo o una pared
+	return not is_on_ceiling  # Verificar si el jugador está en contacto con el suelo
 
 func get_animated_sprite_width():
 	if _animated_sprite.frames:
