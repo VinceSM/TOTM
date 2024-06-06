cf9e90dbff9fa6080615ff040ee9e01345cbbebe
Vicente SM <vsmx427@gmail.com>
2024-06-06 14:55:50 -0300

    Add Collectionable

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,22 +1,7 @@
 extends Control
 
 func _ready():
-	_adjust_buttonP($VBoxContainer/PlayButton, 200, 100, 250, 100)
-	_adjust_buttonQ($VBoxContainer/QuitButton, 200, 100, 250, 100)
-
-func _adjust_buttonP(button, left, top, width, height):
-	button.rect_min_size = Vector2(width, height) 
-	button.margin_left = left
-	button.margin_top = top
-	button.margin_right = left + width
-	button.margin_bottom = top + height
-	
-func _adjust_buttonQ(button, left, top, width, height):
-	button.rect_min_size = Vector2(width, height) 
-	button.margin_left = left
-	button.margin_top = top
-	button.margin_right = left + width
-	button.margin_bottom = top + height
+	pass
 	
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Scenes/Level1.tscn")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -28,12 +28,13 @@ func handle_input():
 		_animated_sprite.flip_h = true  # Voltear sprite hacia la izquierda
 	else:
 		_animated_sprite.stop()
+		_animated_sprite.play("look")
 
 	if Input.is_action_just_pressed("ui_up") and not is_on_ceiling:
 		_animated_sprite.flip_v = true  # Voltear sprite verticalmente (mirar hacia arriba)
 		is_on_ceiling = true
 	elif Input.is_action_just_pressed("ui_down") and is_on_ceiling:
-		_animated_sprite.flip_v = false  # No voltear sprite verticalmente (mirar hacia abajo)
+		_animated_sprite.flip_v = false
 		is_on_ceiling = false
 
 func move_player(delta):
@@ -46,8 +47,16 @@ func move_player(delta):
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 
+func avoid_player_fall(delta):
+	pass
+
 func get_animated_sprite_height():
 	if _animated_sprite.frames:
 		var animation = _animated_sprite.frames.get_animation_names()[0]
 		return _animated_sprite.frames.get_frame(animation, 0).get_size().y
 	return 0
+
+func add_Coin():
+	var canvasLayer = get_tree().get_root().find_node("HUD",true,false)
+	
+	canvasLayer.handleCoinCollected()
