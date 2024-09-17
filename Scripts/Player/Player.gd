d45d9a86239d7156e010489cd905cf5938f0e03c
vsmx427 <vsmx427@gmail.com>
2024-09-17 15:48:29 -0300

    Refactorizacion de Niveles y Jugabilidad

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -2,18 +2,22 @@ extends KinematicBody2D
 
 export var speed = 300
 export var vertical_speed = 400
+export var fall_limit = 2000 
+
 onready var _animated_sprite = $AnimatedSprite
 
 var velocity = Vector2()
 var screen_size
 var is_on_ceiling = false
 var is_on_floor = false
-var is_dead = false
+var is_dead = false  
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func _input(event):
+	if is_dead:
+		return  # Si está muerto, no responderá a la entrada
 	if event.is_action_pressed("ui_up") and not is_on_ceiling:
 		_animated_sprite.flip_v = true 
 		is_on_ceiling = true
@@ -21,10 +25,10 @@ func _input(event):
 		_animated_sprite.flip_v = false
 		is_on_ceiling = false
 		
-
 func _process(_delta):
-	move_player_x()
-	move_player_y()
+	if not is_dead:  # Solo permite mover si el jugador no está muerto
+		move_player_x()
+		move_player_y()
 
 func move_player_x():
 	velocity.x = 0
@@ -42,9 +46,11 @@ func move_player_x():
 		_animated_sprite.play("look")
 
 func move_player_y():
+	# Mover hacia arriba
 	if is_on_ceiling and position.y > 0:
 		velocity.y = -vertical_speed
-	elif not is_on_ceiling and position.y < screen_size.y - get_animated_sprite_height():
+	# Mover hacia abajo (incluyendo por debajo de la pantalla hasta fall_limit)
+	elif not is_on_ceiling and position.y < fall_limit:
 		velocity.y = vertical_speed
 	else:
 		velocity.y = 0
@@ -68,6 +74,8 @@ func add_Portal():
 	canvasLayer.update_display()  
 
 func die():
+	is_dead = true  
+	_animated_sprite.stop()
 	$deadSound.play()
 	$DeadTimer.start()
 
