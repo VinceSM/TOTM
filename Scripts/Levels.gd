10073382d60fbe618c8045042150b88ff60674fe
vsmx427 <vsmx427@gmail.com>
2024-10-07 17:39:37 -0300

    Save Score in process, Levels are finished

--- Scripts/Levels.gd
+++ Scripts/Levels.gd
@@ -1,27 +1,11 @@
 extends Node2D
 
 export (int) var nextLevel
-var song_player = null
-export (int) var time_left = 40 
-
-func set_time(seconds):
-	time_left = seconds
-
-func reduce_time():
-	time_left -= 1
-	return time_left
+onready var song_player = $SongLevel
 
 func _ready():
-	if song_player == null:
-		song_player = $SongLevel
-		song_player.play()
-
-	if not song_player.playing:
-		song_player.play()
+	if song_player:
+		song_player.play()  
 
 func get_next_level():
 	return nextLevel
-	
-
-
-
--- Scripts/Player/ScoreScene.gd
+++ Scripts/Player/ScoreScene.gd
@@ -1,13 +1,48 @@
-extends Node2D
+extends Node
 
+var player_name = ""
+var player_score = 0
+
+const SCORE_FILE_PATH = "user://scores.json"
+const MAX_SCORES = 10  
 
 func _ready():
-	pass # Replace with function body.
-	
+	player_score = GameData.coins  
+	update_display()
+
+func update_display():
+	$ScoreLabel.text = "Score: " + str(player_score)
+
+func _on_btnSave_pressed():
+	player_name = $NameInput.text
+	if player_name == "":
+		print("Debe ingresar un nombre para guardar el score")
+		return
+	save_score(player_name, player_score)
+	#get_tree().change_scene("res://Scenes/Menu/Leaderboard.tscn")
+
+func save_score(name, score):
+	var scores = load_scores() 
+	scores.append({"name": name, "score": score})
+	scores.sort_custom(self, "_sort_scores")
+	if scores.size() > MAX_SCORES:
+		scores = scores.slice(0, MAX_SCORES)
+	var file = File.new()
+	file.open(SCORE_FILE_PATH, File.WRITE)
+	file.store_string(to_json(scores)) 
+	file.close()
+
+func load_scores():
+	var file = File.new()
+	if file.file_exists(SCORE_FILE_PATH):
+		file.open(SCORE_FILE_PATH, File.READ)
+		var data = file.get_as_text()
+		file.close()
+		return parse_json(data) 
+	return []  
+
+func _sort_scores(a, b):
+	return int(b["score"]) - int(a["score"]) 
 
 func _on_btnMainMenu_pressed():
 	get_tree().change_scene("res://Scenes/Menu/Main_Menu.tscn")
-
-
-func _on_btnSave_pressed():
-	pass # Replace with function body.
