cb21fe51afd8e2ab9c4481e5454a8f8f7d1cffb5
vsmx427 <vsmx427@gmail.com>
2024-10-08 00:38:07 -0300

    Game Finished

--- Scripts/Menu/MainMenu.gd
+++ Scripts/Menu/MainMenu.gd
@@ -1,8 +1,5 @@
 extends Control
 
-func _ready():
-	$SongGame.play()
-
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Scenes/Levels/1.tscn")
 
---
+++ Scripts/Singleton/Levels.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+export (int) var nextLevel
+
+
+func get_next_level():
+	return nextLevel
